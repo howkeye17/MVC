48a94caf8bab25f6e8a6f5364157db22c5f87771
howkeye17 <howkeye17@gmail.com>
2021-07-15 15:57:11 +0300

    Initial Commit

---
+++ MVC-N/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Valera Vasilevich on 15.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
